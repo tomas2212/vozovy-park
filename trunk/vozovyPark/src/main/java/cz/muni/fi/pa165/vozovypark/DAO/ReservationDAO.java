7a80573172b40d22205d1832f9bc27996f80d031
krak.eduard@gmail.com <krak.eduard@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-10-09 09:41:53 +0000

    Reservation queries added and ReservationDAO updated

--- trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DAO/ReservationDAO.java
+++ trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DAO/ReservationDAO.java
@@ -4,7 +4,10 @@
  */
 package cz.muni.fi.pa165.vozovypark.DAO;
 
+import cz.muni.fi.pa165.vozovypark.entities.Car;
+import cz.muni.fi.pa165.vozovypark.entities.Employee;
 import cz.muni.fi.pa165.vozovypark.entities.Reservation;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
@@ -19,11 +22,41 @@ public class ReservationDAO {
     public ReservationDAO(EntityManager em) {
         this.entityManager = em;
     }
-    
- 
+   
     public Reservation getReservationById(Long id){
         Query q = entityManager.createNamedQuery(Reservation.FIND_BY_ID);
         q.setParameter("id", id);
         return (Reservation) q.getSingleResult();
     }
+    
+    public void insert(Reservation r) {
+        entityManager.persist(r);
+    }
+    
+    public void update(Reservation r) {
+        entityManager.merge(r);
+    }
+    
+    public void remove(Reservation r) {
+        entityManager.remove(entityManager.merge(r));
+    }
+
+  public List<Reservation> getReservationByCar(Car car) {
+       Query q = entityManager.createNamedQuery(Reservation.FIND_BY_CAR);
+       q.setParameter("car", car);
+      return q.getResultList();
+   }
+   
+   public List<Reservation> getReservationByEmployee(Employee employee) {
+       Query q = entityManager.createNamedQuery(Reservation.FIND_BY_EMPLOYEE);
+       q.setParameter("employee", employee);
+       return q.getResultList();
+   }
+   
+   public List<Reservation> getReservationByCarAndEmployee(Car car, Employee employee) {
+       Query q = entityManager.createNamedQuery(Reservation.FIND_BY_CAR_AND_EMPLOYEE);
+       q.setParameter("car", car);
+       q.setParameter("employee", employee);
+       return q.getResultList();
+   }
 }
