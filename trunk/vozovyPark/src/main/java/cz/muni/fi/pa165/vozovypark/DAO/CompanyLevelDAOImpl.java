0bbafc229f8ba6010e78849f7470e0323e05e440
Andrej.Bauer@gmail.com <Andrej.Bauer@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-10-14 10:49:53 +0000

    Company Level dao empty impl

---
+++ trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DAO/CompanyLevelDAOImpl.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.vozovypark.DAO;
+
+import cz.muni.fi.pa165.vozovypark.entities.CompanyLevel;
+import java.util.List;
+
+/**
+ *
+ * @author andrej
+ */
+public class CompanyLevelDAOImpl implements CompanyLevelDAO{
+
+    public void insert(CompanyLevel companyLevel) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void update(CompanyLevel companyLevel) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void remove(CompanyLevel companyLevel) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public CompanyLevel getCompanyLevelById(Long Id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<CompanyLevel> getAllCompanyLevels() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
