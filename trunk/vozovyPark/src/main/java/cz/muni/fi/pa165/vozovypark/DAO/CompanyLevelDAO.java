9836d539e628379644d942d13100930311bc4157
Andrej.Bauer@gmail.com <Andrej.Bauer@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-10-14 10:10:21 +0000

    Company Level dao interface

---
+++ trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DAO/CompanyLevelDAO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.vozovypark.DAO;
+
+import cz.muni.fi.pa165.vozovypark.entities.CompanyLevel;
+import java.util.List;
+
+/**
+ *
+ * @author andrej
+ */
+public interface CompanyLevelDAO {
+    
+    void insert(CompanyLevel companyLevel);
+    
+    void update(CompanyLevel companyLevel);
+    
+    void remove(CompanyLevel companyLevel);
+    
+    CompanyLevel getCompanyLevelById(Long Id);
+    
+    List<CompanyLevel> getAllCompanyLevels();    
+    
+    
+    
+    
+}
