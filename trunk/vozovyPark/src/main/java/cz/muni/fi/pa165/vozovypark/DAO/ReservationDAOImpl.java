a26c0bd70d174ba55b102bf399ed38376af29d07
krak.eduard@gmail.com <krak.eduard@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-10-14 18:43:21 +0000

    Reservation DAO updated and interface for this DAO created

---
+++ trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DAO/ReservationDAOImpl.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.vozovypark.DAO;
+
+import cz.muni.fi.pa165.vozovypark.entities.Car;
+import cz.muni.fi.pa165.vozovypark.entities.Employee;
+import cz.muni.fi.pa165.vozovypark.entities.Reservation;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Eduard Krak
+ */
+
+public class ReservationDAOImpl implements ReservationDAO {
+    //@PersistenceContext
+    EntityManager entityManager;
+
+    public ReservationDAOImpl(EntityManager em) {
+        this.entityManager = em;
+    }
+   
+    public Reservation getReservationById(Long id){
+        Query q = entityManager.createNamedQuery(Reservation.FIND_BY_ID);
+        q.setParameter("id", id);
+        return (Reservation) q.getSingleResult();
+    }
+    
+    public void insert(Reservation r) {
+        entityManager.persist(r);
+    }
+    
+    public void update(Reservation r) {
+        entityManager.merge(r);
+    }
+    
+    public void remove(Reservation r) {
+        entityManager.remove(entityManager.merge(r));
+    }
+
+  public List<Reservation> getReservationByCar(Car car) {
+       Query q = entityManager.createNamedQuery(Reservation.FIND_BY_CAR);
+       q.setParameter("car", car);
+      return q.getResultList();
+   }
+   
+   public List<Reservation> getReservationByEmployee(Employee employee) {
+       Query q = entityManager.createNamedQuery(Reservation.FIND_BY_EMPLOYEE);
+       q.setParameter("employee", employee);
+       return q.getResultList();
+   }
+   
+   public List<Reservation> getReservationByCarAndEmployee(Car car, Employee employee) {
+       Query q = entityManager.createNamedQuery(Reservation.FIND_BY_CAR_AND_EMPLOYEE);
+       q.setParameter("car", car);
+       q.setParameter("employee", employee);
+       return q.getResultList();
+   }
+   
+   public List<Reservation> getAllReservations() {
+       Query q = entityManager.createQuery(Reservation.FIND_ALL);
+       return q.getResultList();
+   }
+}
