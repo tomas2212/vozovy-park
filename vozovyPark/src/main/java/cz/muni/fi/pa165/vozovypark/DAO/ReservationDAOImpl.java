aa436795ef75d6998cec39d6cbb2e05c2874e9fc
krak.eduard@gmail.com <krak.eduard@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-10-15 11:23:54 +0000

    Entity Manager in ReservationDAO changed to EntityManagerFactory + file for CarDAOTest added

--- vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DAO/ReservationDAOImpl.java
+++ vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DAO/ReservationDAOImpl.java
@@ -9,6 +9,7 @@ import cz.muni.fi.pa165.vozovypark.entities.Employee;
 import cz.muni.fi.pa165.vozovypark.entities.Reservation;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 
 /**
@@ -18,43 +19,50 @@ import javax.persistence.Query;
 
 public class ReservationDAOImpl implements ReservationDAO {
     //@PersistenceContext
-    EntityManager entityManager;
+    EntityManagerFactory entityManagerFactory;
 
-    public ReservationDAOImpl(EntityManager em) {
-        this.entityManager = em;
+    public ReservationDAOImpl(EntityManagerFactory emf) {
+        this.entityManagerFactory = emf;
     }
    
     public Reservation getReservationById(Long id){
+        EntityManager entityManager = this.entityManagerFactory.createEntityManager();
         Query q = entityManager.createNamedQuery(Reservation.FIND_BY_ID);
         q.setParameter("id", id);
         return (Reservation) q.getSingleResult();
     }
     
     public void insert(Reservation r) {
+        EntityManager entityManager = this.entityManagerFactory.createEntityManager();
         entityManager.persist(r);
     }
     
     public void update(Reservation r) {
+        EntityManager entityManager = this.entityManagerFactory.createEntityManager();
         entityManager.merge(r);
     }
     
     public void remove(Reservation r) {
+        EntityManager entityManager = this.entityManagerFactory.createEntityManager();
         entityManager.remove(entityManager.merge(r));
     }
 
   public List<Reservation> getReservationByCar(Car car) {
+      EntityManager entityManager = this.entityManagerFactory.createEntityManager();
        Query q = entityManager.createNamedQuery(Reservation.FIND_BY_CAR);
        q.setParameter("car", car);
       return q.getResultList();
    }
    
    public List<Reservation> getReservationByEmployee(Employee employee) {
+       EntityManager entityManager = this.entityManagerFactory.createEntityManager();
        Query q = entityManager.createNamedQuery(Reservation.FIND_BY_EMPLOYEE);
        q.setParameter("employee", employee);
        return q.getResultList();
    }
    
    public List<Reservation> getReservationByCarAndEmployee(Car car, Employee employee) {
+       EntityManager entityManager = this.entityManagerFactory.createEntityManager();
        Query q = entityManager.createNamedQuery(Reservation.FIND_BY_CAR_AND_EMPLOYEE);
        q.setParameter("car", car);
        q.setParameter("employee", employee);
@@ -62,6 +70,7 @@ public class ReservationDAOImpl implements ReservationDAO {
    }
    
    public List<Reservation> getAllReservations() {
+       EntityManager entityManager = this.entityManagerFactory.createEntityManager();
        Query q = entityManager.createQuery(Reservation.FIND_ALL);
        return q.getResultList();
    }
