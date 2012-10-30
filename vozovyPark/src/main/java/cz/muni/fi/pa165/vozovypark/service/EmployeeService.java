8a3da2e55a65fc39b39ab11000953ff7ec1f313d
Andrej.Bauer@gmail.com <Andrej.Bauer@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-10-30 09:33:39 +0000

    service interfaces

---
+++ vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/service/EmployeeService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.vozovypark.service;
+
+import cz.muni.fi.pa165.vozovypark.DTO.CompanyLevelDTO;
+import cz.muni.fi.pa165.vozovypark.DTO.EmployeeDTO;
+import java.util.List;
+
+
+/**
+ *
+ * @author andrej
+ */
+public interface EmployeeService {
+    
+    EmployeeDTO createEmployee(EmployeeDTO employee);
+    
+    EmployeeDTO updateEmployee(EmployeeDTO employee);
+    
+    EmployeeDTO getEmployeeById(Long id);
+    
+    List<EmployeeDTO> getAllEmployees();
+    
+    List<EmployeeDTO> getEmployeesByCompanyLevel(CompanyLevelDTO companyLevel);
+    
+    
+    
+}
---
+++ vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/service/ReservationService.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.vozovypark.service;
+
+import cz.muni.fi.pa165.vozovypark.DTO.CarDTO;
+import cz.muni.fi.pa165.vozovypark.DTO.EmployeeDTO;
+import cz.muni.fi.pa165.vozovypark.DTO.ReservationDTO;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author andrej
+ */
+public interface ReservationService {
+    
+    ReservationDTO createReservation(Long carId, Long employeeId, Date dateFrom, Date dateTo);
+    
+    ReservationDTO updateReservation(ReservationDTO reservation);
+    
+    ReservationDTO getReservationById(Long id);
+    
+    List<ReservationDTO> getReservationsByEmployee(EmployeeDTO employee);
+    
+    List<ReservationDTO> getReservationsByCar(CarDTO car);
+    
+    List<ReservationDTO> getReservationsByCarAndEmployee(CarDTO car, EmployeeDTO employee);
+    
+    List<ReservationDTO> getReservationsToConfirm();
+    
+    ReservationDTO rentCar(Long reserVationId);
+   
+    ReservationDTO returnCar(Long reservationId);
+    
+    void removeReservation(Long id);
+    
+     
+    
+}
