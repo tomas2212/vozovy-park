aa45550f48fab13f2343dead7eaf434036f9f229
lukas.maticky@gmail.com <lukas.maticky@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-11-04 23:07:29 +0000

    Commenty + metoda remove

--- trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/service/EmployeeService.java
+++ trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/service/EmployeeService.java
@@ -11,16 +11,51 @@ import java.util.List;
  */
 public interface EmployeeService {
     
+    /**
+     * Creates employee in DB
+     * 
+     * @param employee
+     * @return new employee in DB
+     */
     EmployeeDTO createEmployee(EmployeeDTO employee);
     
+   /**
+     * Updates employee in DB
+     * 
+     * @param employee
+     * @return updated employee
+     */
     EmployeeDTO updateEmployee(EmployeeDTO employee);
     
+    /**
+     * Finding employee
+     * 
+     * @param id
+     * @return Employee with its ID
+     */
     EmployeeDTO getEmployeeById(Long id);
     
+    /**
+     * List all employees in DB
+     * 
+     * @return All employees in DB
+     */
     List<EmployeeDTO> getAllEmployees();
-    
+    /**
+     * Listing employees by company level
+     * 
+     * @param companyLevel cl
+     * @return all employees with higher CompanyLevel
+     */
     List<EmployeeDTO> getEmployeesByCompanyLevel(CompanyLevelDTO companyLevel);
     
+    /**
+     * Removing employee from DB
+     * 
+     * @param id employee to remove
+     * @return void
+     */
+    void removeCar(Long id);
     
-    
+        
 }
