03b94e3b308a7379d09a072cfbb8f28bd44c2b13
tomas2212@gmail.com <tomas2212@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-11-04 10:17:20 +0000

    uprava komentarov...

--- trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DTO/CarDTO.java
+++ trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/DTO/CarDTO.java
@@ -1,14 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.vozovypark.DTO;
 
 import java.util.Date;
 
 /**
  *
- * @author andrej
+ * @author Tomas Svrcek
  */
 public class CarDTO {
     private Long id;
--- trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/service/EmployeeService.java
+++ trunk/vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/service/EmployeeService.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.vozovypark.service;
 
 import cz.muni.fi.pa165.vozovypark.DTO.CompanyLevelDTO;
