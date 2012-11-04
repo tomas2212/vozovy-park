4680a01de62568c0cfc6fffc40fa4b4427e4002b
lukas.maticky@gmail.com <lukas.maticky@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-11-04 23:09:07 +0000

    Commenty + metoda remove

--- vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/service/EmployeeService.java
+++ vozovyPark/src/main/java/cz/muni/fi/pa165/vozovypark/service/EmployeeService.java
@@ -55,7 +55,7 @@ public interface EmployeeService {
      * @param id employee to remove
      * @return void
      */
-    void removeCar(Long id);
+    void removeEmployee(Long id);
     
         
 }
