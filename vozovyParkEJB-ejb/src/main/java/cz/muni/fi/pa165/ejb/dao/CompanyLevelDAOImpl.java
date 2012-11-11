a8b5d129e06e4ba448d4ab43004a06f586116a66
Andrej.Bauer@gmail.com <Andrej.Bauer@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-11-11 13:54:35 +0000

    companyLevel part of project

---
+++ vozovyParkEJB-ejb/src/main/java/cz/muni/fi/pa165/ejb/dao/CompanyLevelDAOImpl.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.ejb.dao;
+import cz.muni.fi.pa165.ejb.entities.CompanyLevel;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.ejb.Stateless;
+
+
+/**
+ *
+ * @author Andrej Bauer
+ */
+@Stateless
+public class CompanyLevelDAOImpl implements CompanyLevelDAO {
+    
+    @PersistenceContext
+    protected EntityManager entityManager;
+    
+   
+    public void insert(CompanyLevel companyLevel) {
+        if(companyLevel == null){
+            throw new IllegalArgumentException("you must specify company level");
+        }        
+        entityManager.persist(companyLevel);
+       
+    }
+
+    public void update(CompanyLevel companyLevel) {
+        if(companyLevel == null){
+            throw new IllegalArgumentException("you must specify company level");
+        }
+        if(companyLevel.getId() == null){
+            throw new IllegalArgumentException("cant update not persit entity");
+        }  
+        entityManager.merge(companyLevel);
+        
+    }
+
+    public void remove(CompanyLevel companyLevel) {
+       if(companyLevel == null){
+            throw new IllegalArgumentException("you must specify company level");
+        }
+        if(companyLevel.getId() == null){
+            throw new IllegalArgumentException("cant rentityManagerove not persit entity");
+        } 
+        entityManager.remove(entityManager.merge(companyLevel));
+       
+    }
+
+    public CompanyLevel getCompanyLevelById(Long id) {
+        if(id == null){
+            throw new IllegalArgumentException("you must specify id of company level");
+        }        
+        return entityManager.find(CompanyLevel.class, id);
+        
+    }
+
+    public List<CompanyLevel> getAllCompanyLevels() {        
+        TypedQuery<CompanyLevel> q = entityManager.createQuery("SELECT c FROM CompanyLevel c", CompanyLevel.class);
+        return q.getResultList();
+        
+    }
+
+    public CompanyLevel getRootCompanyLevel() {
+        TypedQuery<CompanyLevel> q = entityManager.createQuery("SELECT c.levelValue FROM CompanyLevel c where c.levelValue = :levelValue", CompanyLevel.class);
+        q.setParameter("levelValue", getMinLevelValue());
+        return q.getSingleResult();
+    }
+    
+    public Integer getMaxLevelValue() {
+        TypedQuery<Integer> q = entityManager.createQuery("SELECT max(c.levelValue) FROM CompanyLevel c", Integer.class);
+        return q.getSingleResult();
+    }
+
+    public Integer getMinLevelValue() {
+        TypedQuery<Integer> q = entityManager.createQuery("SELECT min(c.levelValue) FROM CompanyLevel c", Integer.class);
+        return q.getSingleResult();
+    }
+}
