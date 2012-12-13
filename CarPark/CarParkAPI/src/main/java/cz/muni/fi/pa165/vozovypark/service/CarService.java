3c024cd5c8d14fc3cfedad46e30808091de9d977
tomas2212@gmail.com <tomas2212@gmail.com@effe3e31-081d-01c4-c94d-26a71644c58d>
2012-12-13 15:22:15 +0000

    formaty textov a okraslovacky....

--- CarPark/CarParkAPI/src/main/java/cz/muni/fi/pa165/vozovypark/service/CarService.java
+++ CarPark/CarParkAPI/src/main/java/cz/muni/fi/pa165/vozovypark/service/CarService.java
@@ -9,61 +9,60 @@ import java.util.List;
  * @author Tomas Svrcek
  */
 public interface CarService {
-    
+
     /**
      * Creates car in DB
-     * 
+     *
      * @param car
      * @return new car in DB
      */
     CarDTO createCar(CarDTO car);
-    
+
     /**
      * Updates car in DB
-     * 
+     *
      * @param car
      * @return updated car
      */
     CarDTO updateCar(CarDTO car);
-    
+
     /**
      * Set availability
-     * 
+     *
      * @param id
      * @param available
      * @return Car with new availability
      */
     CarDTO setCarAvailable(Long id, Boolean available);
-    
+
     /**
      * Removing Car from DB
-     * 
+     *
      * @param id car to remove
      * @return void
      */
     void removeCar(Long id);
-    
+
     /**
      * Finding car
-     * 
+     *
      * @param id
      * @return Car with its ID
      */
     CarDTO getCarById(Long id);
-    
+
     /**
      * List all cars in DB
-     * 
+     *
      * @return All cars in DB
      */
     List<CarDTO> getAllCars();
-    
+
     /**
      * Listing cars by company level
-     * 
+     *
      * @param companyLevel cl
      * @return all cars with higher CompanyLevel
      */
     List<CarDTO> getCarsByCompanyLevel(CompanyLevelDTO companyLevel);
-            
 }
--- CarPark/CarParkAPI/src/main/java/cz/muni/fi/pa165/vozovypark/service/CompanyLevelService.java
+++ CarPark/CarParkAPI/src/main/java/cz/muni/fi/pa165/vozovypark/service/CompanyLevelService.java
@@ -8,54 +8,53 @@ import java.util.List;
  * @author Andrej Bauer
  */
 public interface CompanyLevelService {
-    
+
     /**
-     * Creates new Company Level
-     * only parameters is name. CompanyLevel value will be automaticaly genereted as bottom value
+     * Creates new Company Level only parameters is name. CompanyLevel value
+     * will be automatically generated as bottom value
+     *
      * @param CompanyLevelName
      * @return DTO of newly created companyLevel value
      */
     CompanyLevelDTO createCompanyLevel(String CompanyLevelName);
-    
+
     /**
      * Updated existing company Level
+     *
      * @param CopmanyLevelDTO
-     * @return return updated CompanyLevelDTO originally from persistance layer
+     * @return return updated CompanyLevelDTO originally from persistence layer
      */
     CompanyLevelDTO updateCompanyLevel(CompanyLevelDTO companyLevel);
-    
+
     /**
-     * Set existing company level as root
-     * @param id of company level
-     * @return return updated CompanyLevelDTO originally from persistance layer
-     */
-    CompanyLevelDTO setCompanyLevelAsRoot(Long id);
-    
-    /**
-     * Set company tu current position. It moved all other companyLevel if it is nessecery
+     * Set company to current position. It moved all other companyLevel if it is
+     * necessary
+     *
      * @param id
      * @param position
-     * @return return updated CompanyLevelDTO originally from persistance layer
+     * @return return updated CompanyLevelDTO originally from persistence layer
      */
     CompanyLevelDTO setCompanyLevelToPosition(Long id, Integer position);
-    
+
     /**
-     * Gets comapny level by its ID
+     * Gets company level by its ID
+     *
      * @param id
-     * @return return CompanyLevelDTO originally from persistance layer
+     * @return return CompanyLevelDTO originally from persistence layer
      */
     CompanyLevelDTO getCompanyLevelById(Long id);
-    
+
     /**
      * Get all company levels
+     *
      * @return list of company levels
      */
     List<CompanyLevelDTO> getAllCompanyLevels();
-    
+
     /**
-     * Remove company level 
+     * Remove company level
+     *
      * @param id of companyLevel
      */
     void removeCompanyLevel(Long id);
-    
 }
